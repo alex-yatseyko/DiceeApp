71993adfe75e493f10a4618406223f23632f659a
Alex Yatseyko <alex.yatseiko@gmail.com>
2020-07-26 05:39:36 +0300

    Void

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
-  dice() {
+  void dice() {
     setState(() {
       leftDiceNumber = Random().nextInt(6) + 1;
       rightDiceNumber = Random().nextInt(6) + 1;
